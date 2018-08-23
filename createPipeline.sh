95c4ff6fbe4c85d6df083e0e2bab8266d1622304
Luke Pahler <luke.pahler@cgi.com>
2018-08-23 12:05:23 -0600

    Eureka Service (had manually pushed before this)

--- createPipeline.sh
+++ createPipeline.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 CONCOURSE_TARGET=training-ci
 PIPELINE=roster-pipeline
+#PREREQ - be logged in
+# fly login -t training-ci --insecure -c https://training-ci.aws.rscale.io -u luke -p goplatformgo -n luke
 echo Creating pipeline \"$PIPELINE\" in concourse target \"$CONCOURSE_TARGET\" 
 fly -t $CONCOURSE_TARGET set-pipeline -p $PIPELINE -c ../training-pipelines/application/pipeline.yml -l ./ci/config.yml
--- src/main/java/io/rscale/training/CloudAppInfoConfig.java
+++ src/main/java/io/rscale/training/CloudAppInfoConfig.java
@@ -12,12 +12,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.Cloud;
 import org.springframework.cloud.CloudFactory;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.service.ServiceInfo;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+@EnableDiscoveryClient
 @Configuration
 @Profile("cloud")
 public class CloudAppInfoConfig {
